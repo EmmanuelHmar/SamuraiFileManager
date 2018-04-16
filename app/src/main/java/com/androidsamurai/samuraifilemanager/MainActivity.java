15ec6f021d30ca1f210a2b0ce3b4c84336db6c52
EmmanuelHmar <eman.hmar@gmail.com>
2018-04-16 12:09:53 -0400

    added permissions

--- app/src/main/java/com/androidsamurai/samuraifilemanager/MainActivity.java
+++ app/src/main/java/com/androidsamurai/samuraifilemanager/MainActivity.java
@@ -1,13 +1,40 @@
 package com.androidsamurai.samuraifilemanager;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
+
+;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int requestCode = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//        Check to see if read/write permission is granted to app
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+//            Permission not granted
+            Toast.makeText(this, "Denies", Toast.LENGTH_SHORT).show();
+
+//            Request the app for permissions
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    requestCode);
+        }
     }
+
 }
